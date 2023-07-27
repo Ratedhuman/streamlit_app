096228b8680298e367ddeacb1492d4818e3de3c4
Philippe Razmadze <105224339+Ratedhuman@users.noreply.github.com>
2023-07-27 11:21:21 -0400

    Update streamlit_app.py
    a pick list here so they cab pick thr fruit they want to include

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 
-
 streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
@@ -14,6 +13,9 @@ import pandas
 
 # We're going to use a CSV file from that bucket in our app.
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they cab pick thr fruit they want to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 # choose the Fruit Name Column as the index
 my_fruit_list = my_fruit_list.set_index('Fruit')
