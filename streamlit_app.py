451b1e6fa33b199f989ce8f5e5a24c27e1f68e85
Philippe Razmadze <105224339+Ratedhuman@users.noreply.github.com>
2023-07-27 00:16:56 -0400

    Update streamlit_app.py
    addressed error for my_food_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -13,7 +14,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 
 streamlit.header(" 🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
 # pick list here so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit.index))
